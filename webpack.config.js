122197b7e4b722cfda97e9213e07d60c91d56a5f
Raman <ramandeep1794@gmail.com>
2018-01-17 11:49:31 +0530

    required packages are installed

--- webpack.config.js
+++ webpack.config.js
@@ -11,14 +11,48 @@ var config = {
 
    module: {
       loaders: [
-         {
+        {
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-               presets: ['es2015', 'react']
+               presets: ['react','es2015', "stage-0"]
             }
-         }
+        },
+        {
+          test: /(\.css)$/,
+          loaders: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+            loader: "css-loader" // translates CSS into CommonJS
+          }, {
+            loader: "sass-loader" // compiles Sass to CSS
+          }]
+        },
+        {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file-loader'
+        },
+        {
+          test: /\.(woff|woff2)$/,
+          loader: 'url-loader?prefix=font/&limit=5000'
+        },
+        {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+        },
+        {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        },
+        {
+          test: /\.(gif|jpe?g|png|ico)$/,
+          loader: 'url-loader?limit=10000'
+        }
       ]
    }
 }
