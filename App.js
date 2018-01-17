2ce0976be982984f79fd772705c3572b2ca32ff7
Raman <ramandeep1794@gmail.com>
2018-01-17 12:47:17 +0530

    Revert "App.js having component which will render on index page"
    This reverts commit 81e8e19cb15e0a89f366106ec261aa34c9798bd5.

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import CardsIndexComponent from "./src/components/cardIndexComponent.js";//cards displaying on index page
-
 class App extends React.Component {
-   constructor(props){
-       super(props);
-   }
    render() {
       return (
-            <MuiThemeProvider>
-                <div style={{marginTop:"35px"}}>
-                    <CardsIndexComponent data={this.props.app} />
-                </div>
-            </MuiThemeProvider>
+         <div>
+            Hello World!!!
+         </div>
       );
    }
 }
